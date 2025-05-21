4d81b0059ec54d8242ba1767d8c0ab20aab85871
Damon A. Heath <daheath2007@gmail.com>
2025-05-21 09:59:10 -0500

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 APP_NAME="twiinz-beard-website"
+HEROKU_IMAGE_TAG="registry.heroku.com/$APP_NAME/web" # Define it once
 
 echo "Logging into Heroku container registry..."
 heroku container:login
@@ -8,25 +9,32 @@ heroku container:login
 echo "Setting Heroku stack to container..."
 heroku stack:set container -a $APP_NAME
 
-echo "Building new Docker image (linux/amd64)..."
-docker build --platform=linux/amd64 --no-cache -t main:latest .
+echo "Building new Docker image (linux/amd64) directly with Heroku tag..."
+# Build directly with the Heroku tag. The :latest is implicit if not specified.
+docker build --platform=linux/amd64 --no-cache -t $HEROKU_IMAGE_TAG .
 
-echo "Tagging Docker image..."
-docker tag main:latest registry.heroku.com/$APP_NAME/web
+# The 'docker tag' command is no longer needed as we built with the correct tag.
+# echo "Tagging Docker image..."
+# docker tag main:latest $HEROKU_IMAGE_TAG # REMOVE THIS LINE
 
 echo "Pushing Docker image..."
-docker push registry.heroku.com/$APP_NAME/web
+docker push $HEROKU_IMAGE_TAG # This will push registry.heroku.com/twiinz-beard-website/web:latest
 
 echo "Releasing Docker container..."
+# Heroku container:release web will look for the 'web' image type, which by default is tagged 'latest'
+# which we just pushed.
 heroku container:release web -a $APP_NAME
 
 echo "Checking if web dyno is running..."
-DYNO_COUNT=$(heroku ps -a $APP_NAME | grep web | wc -l)
+# Give a few seconds for the release to potentially start a dyno
+sleep 5 
+DYNO_COUNT=$(heroku ps -a $APP_NAME --json | jq '.[] | select(.type=="web")' | jq -s 'length')
+
 if [ "$DYNO_COUNT" -eq "0" ]; then
   echo "No web dyno running. Scaling web dyno to 1..."
   heroku ps:scale web=1 -a $APP_NAME
 else
-  echo "Web dyno already running."
+  echo "Web dyno already running or starting."
 fi
 
 echo "Checking process status..."
