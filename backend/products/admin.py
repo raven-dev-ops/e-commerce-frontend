058550229d706e5e440ffa9e822e1048c1289fb3
Damon A. Heath <daheath2007@gmail.com>
2025-05-22 03:34:01 -0500

    Update
    Split project into FRONTEND repo and BACKEND repo

--- backend/products/admin.py
+++
@@ -1,5 +0,0 @@
-from django_mongoengine.mongo_admin.sites import site as mongo_admin_site
-from .models import Product
-from django_mongoengine.mongo_admin.options import DocumentAdmin
-
-mongo_admin_site.register(Product)
--- backend/users/admin.py
+++
@@ -1,4 +0,0 @@
-# users/admin.py
-from .models import User
-from django_mongoengine.mongo_admin.sites import site as mongo_admin_site
-mongo_admin_site.register(User)
