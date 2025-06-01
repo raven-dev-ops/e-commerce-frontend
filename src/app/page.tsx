66578453384e79ab7cb7000efd1acfc102aa290c
gptfleet <gptfleet@gmail.com>
2025-06-01 01:15:30 +0000

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ const Page: React.FC = () => {
   return (
     <Layout>
       <div className="home-background" />
-      <div className="flex flex-1 items-center justify-center w-full h-[calc(100vh-64px-64px)]"> 
-        {/* Replace 64px with your navbar/footer height if fixed, or remove if they're static */}
+      <div className="flex flex-1 items-center justify-center w-full h-[calc(100vh-64px)]"> 
         <h1 className="text-3xl text-white text-center drop-shadow-lg">
           Under Construction
         </h1>
